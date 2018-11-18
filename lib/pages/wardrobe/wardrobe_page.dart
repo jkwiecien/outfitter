ca86824e44969c73bbd8da88d299e9372875b2b9
Jacek Kwiecień <jacek.kwiecien@schibsted.com>
2018-11-18 14:17:24 +0100

    adds wardrobe page

---
+++ lib/pages/wardrobe/wardrobe_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WardrobePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _WardrobePageState();
+}
+
+class _WardrobePageState extends State<WardrobePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
